979d70e6f20a54cbb3414719985222e5fa35a4ac
Vladimir Kidyaev <krios2146@gmail.com>
2024-10-17 01:08:44 +0700

    feat: add error handling for TCP socket creation

--- lib/server/server.ml
+++ lib/server/server.ml
@@ -1,24 +1,41 @@
 let accepting_connections = ref true
 
 let create_server_socket port =
-  let module U = Unix in
-  let socket_address = U.ADDR_INET (U.inet_addr_loopback, port) in
-  let socket = U.socket U.PF_INET U.SOCK_STREAM 0 in
+  try
+    let module U = Unix in
+    let socket_address = U.ADDR_INET (U.inet_addr_loopback, port) in
+    let socket = U.socket U.PF_INET U.SOCK_STREAM 0 in
 
-  U.setsockopt socket U.SO_REUSEADDR true;
-  U.bind socket socket_address;
-  U.listen socket 5;
+    U.setsockopt socket U.SO_REUSEADDR true;
+    U.bind socket socket_address;
+    U.listen socket 5;
 
-  socket
+    Some socket
+  with
+  | Unix.Unix_error (Unix.EADDRINUSE, _, _) ->
+      Log.error (fun m ->
+          m "Unable to bind TCP socket, port %d already in use" port);
+      None
+  | _ ->
+      Log.error (fun m ->
+          m "Unknown error occured while creating server socket on port %d" port);
+      None
+
+let handle_tcp_client_data data =
+  Log.debug (fun m -> m "Recieved data:\n%s" data);
+  data
 
 let handle_client socket =
   let buffer = Bytes.create 1024 in
   let received_bytes = Unix.read socket buffer 0 1024 in
 
-  if received_bytes > 0 then (
-    let data = Bytes.sub_string buffer 0 received_bytes in
-    Log.debug (fun m -> m "Recieved data:\n%s" data);
-    ignore (Unix.write socket buffer 0 received_bytes));
+  (if received_bytes > 0 then
+     let data = Bytes.sub_string buffer 0 received_bytes in
+     let handeled_data = handle_tcp_client_data data in
+     let handeled_data_bytes = Bytes.of_string handeled_data in
+     ignore
+       (Unix.write socket handeled_data_bytes 0
+          (Bytes.length handeled_data_bytes)));
 
   Unix.close socket
 
@@ -47,7 +64,10 @@ let accept_connection server_socket =
 
 let run_server port =
   let server_socket = create_server_socket port in
-  Log.info (fun m -> m "Server listening on port %d" port);
-  accept_connection server_socket
+  match server_socket with
+  | Some socket ->
+      Log.info (fun m -> m "Server listening on port %d" port);
+      accept_connection socket
+  | None -> ()
 
 let stop_server () = accepting_connections := false
